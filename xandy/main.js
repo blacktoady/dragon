dea8ffa1036a86f4c5ce614f7c21a3d5c7008780
anatolii_lycan <turbolycanthrope@icloud.com>
2020-12-03 15:29:44 +0200

    add details to XandY

--- xandy/main.js
+++ xandy/main.js
@@ -2,10 +2,16 @@ function hello() {
     alert("нажми где нибудь, чтобы переместить меня))");
 }
 
+let lastPos = 0;
+
 function move(evt) {
-    console.log(evt.clientX);
-    console.log(evt.clientY);
     dragon = document.querySelector('#drg');
+    if (lastPos < evt.clientX) {
+        dragon.style.transform = "scale(1, 1)";
+    }   else {
+        dragon.style.transform = "scale(-1, 1)";
+    }
     dragon.style.left = evt.clientX + "px";
     dragon.style.top = evt.clientY + "px";
+    lastPos = evt.clientX;
 }
