2f50df06f52b5b99ec35380a0bab9ba81d0dde7c
anatolii_lycan <turbolycanthrope@icloud.com>
2020-12-03 15:14:01 +0200

    add functional

--- onlyx/main.js
+++ onlyx/main.js
@@ -2,7 +2,15 @@ function hello() {
     alert("нажми где нибудь, чтобы переместить меня))");
 }
 
+let lastPos = 0;
+
 function move(evt) {
     dragon = document.querySelector('#drg');
+    if (lastPos < evt.clientX) {
+        dragon.style.transform = "scale(1, 1)";
+    }   else {
+        dragon.style.transform = "scale(-1, 1)";
+    }
     dragon.style.left = evt.clientX + "px";
+    lastPos = evt.clientX;
 }
